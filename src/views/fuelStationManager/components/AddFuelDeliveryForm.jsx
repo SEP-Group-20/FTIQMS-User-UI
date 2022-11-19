1eafd15caf88aa8550e7111abe193776fdd21417
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2022-11-19 11:21:58 +0530

    Bug fixes

--- src/views/fuelStationManager/components/AddFuelDeliveryForm.jsx
+++ src/views/fuelStationManager/components/AddFuelDeliveryForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Slide from "@mui/material/Slide";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -178,12 +177,6 @@ function AddFuelDeliveryForm() {
   return (
     <Box bgcolor="#d1cebd" flex={5} p={2} >
       <Box bgcolor="#f5f4f0" flex={5} p={3} sx={{ borderRadius: '9px' }}>
-        <Slide
-          direction={checked ? "up" : "down"}
-          in={checked}
-          mountOnEnter
-          unmountOnExit
-        >
           <div>
             <ThemeProvider theme={theme}>
               <Container component="main" maxWidth="xs">
@@ -319,7 +312,6 @@ function AddFuelDeliveryForm() {
               </Container>
             </ThemeProvider>
           </div>
-        </Slide>
       </Box>
     </Box>
   );
